835c3affc2ca47294ea65e79c05413e65991b180
Nicholas J. Michalek <djphazer@gmail.com>
2022-10-09 09:28:44 -0400

    Replace AnnularFusion with EuclidX
    Redesigned by qiemem and djphazer
    Assignable CV input mod based on adegani's Euclid applet design

--- software/o_c_REV/hemisphere_config.h
+++ software/o_c_REV/hemisphere_config.h
@@ -17,7 +17,6 @@
 #define HEMISPHERE_APPLETS { \
     DECLARE_APPLET(  8, 0x01, ADSREG), \
     DECLARE_APPLET( 34, 0x01, ADEG), \
-    DECLARE_APPLET( 15, 0x02, AnnularFusion), \
     DECLARE_APPLET( 47, 0x09, ASR), \
     DECLARE_APPLET( 56, 0x10, AttenuateOffset), \
     DECLARE_APPLET( 41, 0x41, Binary), \
@@ -36,6 +35,7 @@
     DECLARE_APPLET(  9, 0x08, DualQuant), \
     DECLARE_APPLET( 45, 0x02, EnigmaJr), \
     DECLARE_APPLET( 42, 0x11, EnvFollow), \
+    DECLARE_APPLET( 15, 0x02, EuclidX), \
     DECLARE_APPLET( 29, 0x04, GateDelay), \
     DECLARE_APPLET( 17, 0x50, GatedVCA), \
     DECLARE_APPLET( 16, 0x80, LoFiPCM), \
